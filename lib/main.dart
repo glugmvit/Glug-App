2afed48e97da5ddfa2e3748f0071caf9c4f458fe
reocoder <mukherjeeshreoshee@gmail.com>
2020-07-18 00:01:05 +0530

    updated the splash screen and login ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:glugapp/screens/login.dart';
-
+import 'package:rive/rive.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:page_transition/page_transition.dart';
@@ -8,6 +8,7 @@ import 'package:animated_splash/animated_splash.dart';
 import 'package:custom_splash/custom_splash.dart';
 import 'dart:async';
 import 'package:glugapp/screens/UserScreen.dart';
+import 'package:flare_flutter/flare_actor.dart';
 
 
 void main() {
@@ -19,8 +20,7 @@ void main() {
         '/WelcomeScreen': (BuildContext context) => new LoginPage(),
       },
     ));
-
-  }
+}
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => new _SplashScreenState();
@@ -30,10 +30,10 @@ class _SplashScreenState extends State<SplashScreen> {
 
   FirebaseUser user;
   FirebaseAuth auth = FirebaseAuth.instance;
-
+  String _animationName="walk";
 
   startTime() async {
-    var _duration = new Duration(seconds: 3);
+    var _duration = new Duration(seconds: 4);
     return new Timer(_duration, navigationPage);
   }
 
@@ -53,81 +53,29 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     startTime();
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Container(
-            width:MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-
-            child: Image.asset("assets/images/blue.png",fit: BoxFit.fill,),
-
-
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
+      body: Container(
+        child:Center(
+          child:Column(
+            children:<Widget>[
               Expanded(
-                flex: 2,
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      CircleAvatar(
 
-                        backgroundColor: Colors.white,
-
-                        radius: 70.0,
-                        child: Container(child: Image.asset("assets/images/logo.png"),
-                         height:130 ,
-                         width: 70),
-
-
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 10.0),
-                      ),
-                      Text(
-                         " ",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24.0),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    CircularProgressIndicator(backgroundColor: Colors.white,),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20.0),
-                    ),
-                    Text(
-                      "Powered By Glug MVIT",
-                      softWrap: true,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0,
-                          color: Colors.white),
-                    )
-                  ],
-                ),
+                child:FlareActor(
+                    "assets/animations/splash_cool.flr",
+                    alignment:Alignment.center,
+                    fit:BoxFit.contain,
+                    animation:_animationName,
+                )
               )
-            ],
+            ]
           )
-        ],
-      ),
+        )
+      )
     );
   }
 }
